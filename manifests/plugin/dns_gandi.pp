4678716e84a833cf22df81e732dda688f2783b9b
Cédric Laudrel <cedric.laudrel@iguanesolutions.com>
2022-05-19 23:50:25 +0200

    add gandi plugin

---
+++ manifests/plugin/dns_gandi.pp
@@ -0,0 +1,42 @@
+# @summary Installs and configures the dns-gandi plugin
+#
+class letsencrypt::plugin::dns_gandi (
+  Optional[String[1]] $package_name = undef,
+  Optional[String[1]] $api_key      = undef,
+  Optional[String[1]] $sharing_id   = undef,
+  Stdlib::Absolutepath $config_path = "${letsencrypt::config_dir}/gandi.ini",
+  Boolean $manage_package           = true,
+  Integer $propagation_seconds      = 10,
+) {
+  require letsencrypt::install
+
+  if ! $api_key {
+    fail('No authentication method provided, please specify api_key.')
+  }
+
+  if $manage_package {
+    if ! $package_name {
+      fail('No package name provided for certbot dns gandi plugin.')
+    }
+
+    package { $package_name:
+      ensure => installed,
+    }
+  }
+
+  $ini_vars = {
+    "certbot_plugin_gandi:dns_api_key"    => $api_key,
+    "certbot_plugin_gandi:dns_sharing_id" => $sharing_id,
+  }
+
+
+  file { $config_path:
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    content => epp('letsencrypt/ini.epp', {
+        vars => { '' => $ini_vars },
+    }),
+  }
+}
